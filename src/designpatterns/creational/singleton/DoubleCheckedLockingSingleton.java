7e8e5811ff91e76a3232741404e9f6a0002de945
Mahammad <ilyazovmehemmed@gmail.com>
2023-08-05 16:20:30 +0400

    Singleton pattern new examples added

---
+++ src/designpatterns/creational/singleton/DoubleCheckedLockingSingleton.java
@@ -0,0 +1,20 @@
+package designpatterns.creational.singleton;
+
+public class DoubleCheckedLockingSingleton {
+    private static DoubleCheckedLockingSingleton instance;
+
+    private DoubleCheckedLockingSingleton() {
+
+    }
+
+    public static DoubleCheckedLockingSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DoubleCheckedLockingSingleton.class) {
+                if (instance == null) {
+                    instance = new DoubleCheckedLockingSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
--- src/designpatterns/creational/singleton/Main.java
+++ src/designpatterns/creational/singleton/Main.java
@@ -2,6 +2,10 @@ package designpatterns.creational.singleton;
 
 public class Main {
     public static void main(String[] args) {
-        Singleton singleton=Singleton.getInstance();
+        EagerInitializationSingleton eager = EagerInitializationSingleton.getInstance();
+
+        LazyInitializedSingleton lazy = LazyInitializedSingleton.getInstance();
+
+        DoubleCheckedLockingSingleton doubleChecked = DoubleCheckedLockingSingleton.getInstance();
     }
 }
